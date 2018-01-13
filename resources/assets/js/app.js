221aa9278f497889d2c18afaf3ffa07a6b370812
paprikaLang <3060866223@qq.com>
2018-01-13 23:09:27 +0800

    加载数据库数据呈现invoices列表

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -4,6 +4,12 @@
 window.Vue = require('vue');
 import App from './App.vue';
 import router from './router';
+
+Vue.filter('formatMoney',(value)=> {
+    return Number(value)
+        .toFixed(2)
+        .replace(/(\d)(?=(\d{3})+(?!\d))/g,"$1,")
+})
 const app = new Vue({
     el: '#root',
     render: h => h(App),
